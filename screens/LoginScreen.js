d744b83b0dc7dd721830724041a050905029eb67
Mandeep Neupane <30949446+np-mndp@users.noreply.github.com>
2024-09-21 21:59:30 -0400

    Login/Signup screen done. Firebase Auth not connected yet

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -7,107 +7,46 @@ import {
   TouchableOpacity,
   StyleSheet,
 } from "react-native";
+import commonStyles from "../assets/styles";
 
-const LoginScreen = () => {
+const LoginScreen = ({ navigation }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   return (
-    <View style={styles.container}>
-      <View style={styles.logoContainer}>
-        <Text style={styles.logo}> 🪑 </Text>
+    <View style={commonStyles.container}>
+      <View style={commonStyles.hero}>
+        <Text style={commonStyles.heroText}>Sign In</Text>
+        <Text style={commonStyles.logo}> 🪑 </Text>
       </View>
-      <View style={styles.formContainer}>
-        <Text style={styles.label}>E-mail:</Text>
+      <View style={commonStyles.formContainer}>
+        <Text style={commonStyles.label}>E-mail:</Text>
         <TextInput
-          style={styles.input}
+          style={commonStyles.input}
           placeholder="Email"
           value={email}
           onChangeText={(text) => setEmail(text)}
         />
-        <Text style={styles.label}>Password:</Text>
+        <Text style={commonStyles.label}>Password:</Text>
         <TextInput
-          style={styles.input}
+          style={commonStyles.input}
           placeholder="Password"
           secureTextEntry={true}
           value={password}
           onChangeText={(text) => setPassword(text)}
         />
         <TouchableOpacity
-          style={styles.button}
+          style={commonStyles.button}
           onPress={() => console.log("Login button pressed")}
         >
-          <Text style={styles.buttonText}>Login</Text>
+          <Text style={commonStyles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate("Signup")}>
+          <Text style={commonStyles.goBack}>Signup</Text>
         </TouchableOpacity>
       </View>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    width: "100%",
-    justifyContent: "center",
-    flexDirection: "column",
-    alignItems: "center",
-    backgroundColor: "#FFF",
-  },
-  label: {
-    fontSize: 16,
-    color: "#000",
-    marginBottom: 5,
-    textAlign: "left",
-    alignSelf: "flex-start",
-    paddingLeft: "10%",
-    fontFamily: "monospace",
-    color: "#0F0F0F",
-  },
-  logoContainer: {
-    alignItems: "center",
-    marginBottom: 20,
-  },
-  logo: {
-    fontWeight: "bold",
-    fontSize: 150,
-    resizeMode: "contain",
-  },
-  formContainer: {
-    paddingHorizontal: 20,
-    // flex:1,
-    flexDirection: "column",
-    width: "100%",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  input: {
-    height: 40,
-    width: "80%",
-    borderColor: "#ccc",
-    borderWidth: 1,
-    paddingHorizontal: 10,
-    paddingVertical: 10,
-    marginBottom: 20,
-    borderRadius: 10, 
-    backgroundColor: "#fff",
-    // Elevation property for Android
-    elevation: 10,  // The higher the value, the stronger the shadow
-  },
-  button: {
-    backgroundColor: "#009c5b",
-    width: "80%",
-    padding: 10,
-    marginTop:25,
-    borderRadius: 5,
-    alignItems: "center",
-    // Elevation property for Android
-    elevation: 10,  // The higher the value, the stronger the shadow
-  },
-  buttonText: {
-    color: "#fff",
-    fontSize: 16,
-    fontWeight:'bold'
-  },
-});
 
 export default LoginScreen;
