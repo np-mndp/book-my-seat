5a51a64af33fb878841a51a0b48b6f02bbb66949
Biraj Gautam <birajgtm@gmail.com>
2024-11-27 21:01:19 -0500

    added slider and app.js update

--- App.js
+++ App.js
@@ -1,92 +1,117 @@
-// App.js
 import "./handlers/gesture-handler";
-import React from "react";
+import LoginScreen from "./screens/LoginScreen";
+import RestaurantDetailScreen from "./screens/RestaurantDetailScreen";
+import SignupScreen from "./screens/SignupScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-
-import store, { persistor } from "./configs/Store";
-import LoginScreen from "./screens/LoginScreen";
-import SignupScreen from "./screens/SignupScreen";
-// import RestaurantListScreen from "./screens/RestaurantListScreen";
-// import RestaurantDetailScreen from "./screens/RestaurantDetailScreen";
-// import BookingScreen from "./screens/BookingScreen";
-// import BookingHistoryScreen from "./screens/BookingHistoryScreen";
-// import ReservationConfirmation from "./screens/ReservationConfirmation";
+import RestaurantListScreen from "./screens/RestaurantListScreen";
 import TabViewScreen from "./screens/TabViewScreen";
+import { Provider, useSelector } from "react-redux";
+import store, { persistor } from "./configs/Store";
+import { PersistGate } from "redux-persist/integration/react";
+import BookingScreen from "./screens/BookingScreen";
+import BookingHistoryScreen from "./screens/BookingHistoryScreen";
+
+import ReservationConfirmation from "./screens/ReservationConfirmation";
 
 const Stack = createStackNavigator();
 
-export default function App() {
+export default function Root() {
   return (
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <NavigationContainer>
-          <Stack.Navigator
-            initialRouteName="Login" // Starting screen
-            screenOptions={{
-              headerStyle: {
-                backgroundColor: "#009c5b",
-              },
-              headerTintColor: "#fff",
-              headerTitleStyle: {
-                fontWeight: "bold",
-              },
-            }}
-          >
-            {/* Authentication Screens */}
-            <Stack.Screen
-              name="Login"
-              component={LoginScreen}
-              options={{ headerTitle: "" }}
-            />
-            <Stack.Screen
-              name="Signup"
-              component={SignupScreen}
-              options={{ headerTitle: "" }}
-            />
-
-            {/* Tab View for Main Screens */}
-            <Stack.Screen
-              name="TabView"
-              component={TabViewScreen}
-              options={{ headerShown: false }}
-            />
-
-            {/* Additional Screens */}
-            {/* <Stack.Screen
-              name="Restaurant List"
-              component={RestaurantListScreen}
-              options={{ headerTitle: "Restaurants" }}
-            />
-            <Stack.Screen
-              name="Restaurant Details"
-              component={RestaurantDetailScreen}
-              options={({ route }) => ({
-                title: route.params?.restaurantData?.title || "Details",
-              })}
-            />
-            <Stack.Screen
-              name="Booking Screen"
-              component={BookingScreen}
-              options={({ route }) => ({
-                title: route.params?.restaurant?.title || "Bookings",
-              })}
-            />
-            <Stack.Screen
-              name="Booking History"
-              component={BookingHistoryScreen}
-              options={{ headerTitle: "My Bookings" }}
-            />
-            <Stack.Screen
-              name="Reservation Confirmation"
-              component={ReservationConfirmation}
-              options={{ headerTitle: "Reservation Confirmation" }}
-            /> */}
-          </Stack.Navigator>
-        </NavigationContainer>
+      <PersistGate persistor={persistor}>
+        <App />
       </PersistGate>
     </Provider>
+  );
+}
+
+function App() {
+  let { user, token } = useSelector((state) => state.auth);
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName={user ? "TabView" : "Login"} // Set HomeScreen as the initial route
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#009c5b",
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+      >
+        <Stack.Screen
+          name="TabView"
+          component={TabViewScreen}
+          options={{
+            headerTitle: "Book My Seat",
+          }}
+        />
+        {/* Other Screens */}
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{
+            headerTitle: "",
+          }}
+        />
+        <Stack.Screen
+          name="Signup"
+          component={SignupScreen}
+          options={{
+            headerTitle: "",
+          }}
+        />
+        <Stack.Screen
+          name="Restaurant List"
+          component={RestaurantListScreen}
+          options={{
+            headerTitle: "Restaurants",
+          }}
+        />
+        <Stack.Screen
+          name="Booking Screen"
+          component={BookingScreen}
+          options={[
+            ({ route }) => ({
+              title: route.params?.restaurant?.title || "Bookings",
+            }),
+            {
+              headerTitle: "Bookings",
+            },
+          ]}
+        />
+
+        <Stack.Screen
+          name="Restaurant Details"
+          component={RestaurantDetailScreen}
+          options={[
+            ({ route }) => ({
+              title: route.params?.restaurantData?.title || "Details",
+            }),
+            {
+              headerTitle: "Details",
+            },
+          ]}
+        />
+        <Stack.Screen
+          name="Booking History"
+          component={BookingHistoryScreen}
+          options={{
+            headerTitle: "My Bookings",
+          }}
+        />
+        <Stack.Screen
+          name="Reservation Confirmation"
+          component={ReservationConfirmation}
+          options={{
+            headerTitle: "Reservation Confirmation",
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
