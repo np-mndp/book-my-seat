9212fa9aa682edc782383531e3ed60c451d720a9
Biraj Gautam <birajgtm@gmail.com>
2024-11-30 00:55:39 -0500

    changes for build

--- babel.config.js
+++ babel.config.js
@@ -4,13 +4,8 @@ module.exports = function(api) {
     presets: ['babel-preset-expo'],
     plugins: [
       [
-        'module:react-native-dotenv',
-        {
-          moduleName: '@env',
-          path: '.env',
-          safe: false,
-          allowUndefined: true,
-        },
+        
+        'react-native-reanimated/plugin'
       ],
     ],
   }
